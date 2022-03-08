f605e50eb872538ab1481d9f642896e86a0dd5d8
VictoriaCardos <vvjcerque@hotmail.com>
2022-03-07 22:25:46 -0300

    keep tryng

--- exercicio16/exercicio16.js
+++ exercicio16/exercicio16.js
@@ -1,14 +1,12 @@
 //16) Implemente um método divida um array por uma quantidade passada por parâmetro.Entrada do método ([1, 2, 3, 4, 5], 2), Resultado do método: [[1, 2], [3, 4], [5]]
 
 //ainda não funciona
-
 function agroupArr(arr, param){
   var newArr = []
   let cont = Math.floor( arr.length / param)
   console.log(cont)
-  arr = JSON.stringify(arr)
-  return arr.split(',',cont)
-  
+
+  return arr.toString().split(',',cont)
   
 }
 
